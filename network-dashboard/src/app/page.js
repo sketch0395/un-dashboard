d41e5660a80af15791c522be8f79a278dd06a17a
Ronnie Nelson <33059841+sketch0395@users.noreply.github.com>
2025-03-18 12:24:00 -0500

    moved to cards
    Moved to cards

--- network-dashboard/src/app/page.js
+++ network-dashboard/src/app/page.js
@@ -1,31 +1,65 @@
 import Dropit from "@/app/components/usermenu";
 import DockerStatus from "./components/dockerstatus";
 import { Button } from "flowbite-react";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 
 export default function Home() {
   return (
     <>
-
       <div className="p-5 flex">
-        <Button href="https://localhost:9200" className="rounded-2xl text-black border-green-400 border-4 border-double h-16 w-56 items-center justify-items-center place-content-center flex">
-          <h1>Elastic</h1>
-        </Button>
-        <div className="p-5 flex">
-          <h1 className="text-xl font-bold pr-5">Elastic Container Status</h1>
-          <DockerStatus containerId="es02" />
+        <div className="p-4">
+          <div className="p-1 rounded-2xl bg-gradient-to-b from-blue-400 to-fuchsia-500">
+            <Card className={"w-64 justify-items-center place-content-center"}>
+              <CardTitle className={"pl-4 text-xl font-bold"}>
+                Elastic logo here
+              </CardTitle>
+
+              <CardContent>
+                <h1 className="font-bold">Container Status</h1>
+                <DockerStatus containerId="es02" />
+              </CardContent>
+              <div className="p-5">
+                <Button
+                  href="https://localhost:9200"
+                  className="flex text-black"
+                >
+                  Launch Elastic
+                </Button>
+
+              </div>
+            </Card>
+          </div>
+        </div>
+        <div className="p-4">
+          <div className="p-1 rounded-2xl bg-gradient-to-b from-blue-400 to-fuchsia-500">
+            <Card className={"w-64 justify-items-center place-content-center"}>
+              <CardTitle className={"pl-4 text-xl font-bold"}>
+                Kibana logo here
+              </CardTitle>
+
+              <CardContent>
+                <h1 className="font-bold">Container Status</h1>
+                <DockerStatus containerId="kib02" />
+              </CardContent>
+              <div className="p-5">
+                <Button
+                  href="https://localhost:5601"
+                  className="flex text-black"
+                >
+                  Launch Kibana
+                </Button>
+              </div>
+            </Card>
+          </div>
         </div>
       </div>
-      <div className="p-5 flex">
-        <Button href="http://localhost:5601" className="rounded-2xl text-black border-blue-500 border-4 border-double h-16 w-56 items-center justify-items-center place-content-center">
-          <h1>Kibana</h1>
-        </Button>
-        <div className="p-5 flex">
-          <h1 className="text-xl font-bold pr-5">Kibana Container Status</h1>
-          <DockerStatus containerId="kib02" />
-        </div>
-      </div>
-
-
     </>
   );
 }
