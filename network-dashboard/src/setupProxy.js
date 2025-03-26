e93556e6f7bccf4e1b9efc5029f621d636110587
Ronnie Nelson <33059841+sketch0395@users.noreply.github.com>
2025-03-26 09:40:47 -0500

    Update setupProxy.js

--- network-dashboard/src/setupProxy.js
+++ network-dashboard/src/setupProxy.js
@@ -1,14 +1,14 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
-    app.use(
-        '/api',
-        createProxyMiddleware({
-            target: 'http://localhost:80',
-            changeOrigin: true,
-            pathRewrite: {
-                '^/api': '', // remove /api prefix when forwarding to the target
-            },
-        })
-    );
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'http://localhost:80',
+      changeOrigin: true,
+      pathRewrite: {
+        '^/api': '', // remove /api prefix when forwarding to the backend
+      },
+    })
+  );
 };
